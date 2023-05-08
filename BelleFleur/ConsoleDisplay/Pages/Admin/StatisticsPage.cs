d1d66a7830ca69efa3e7a84624daa909e5eb41a0
Poliaboy <szpakiewicz.alex@gmail.com>
2023-05-08 23:28:40 +0200

    STAT AND STOCK IMPLEMENTATION

--- BelleFleur/ConsoleDisplay/Pages/Admin/StatisticsPage.cs
+++ BelleFleur/ConsoleDisplay/Pages/Admin/StatisticsPage.cs
@@ -12,15 +12,37 @@ public class StatisticsPage : Menu
         _activeUser = user;
         Description = "Voici les statistiques :";
         Options = new List<Option>()
+        
         {
-            new("Moyenne des ventes par magasin", MeanSales),
+            new("Moyenne du prix d'une commande ", ()=>ShowStatisticMeanAll()),
+            new("Moyenne des prix d'une commande par magasin", ()=>ShowStatisticMean()),
             new("Retour", ExitMenu)
         };
     }
     
-    public void MeanSales()
+    public void ShowStatisticMeanAll()
     {
-        Database.Database.MeanPriceCommand();
+        
+        Options[Index] = new Option("La moyenne du prix d'une commande est de " + Database.Database.MeanPriceAllCommand() + " euros", ()=>HideStatisticMeanAll());
+        Invoke();
+    }
+    
+    public void HideStatisticMeanAll()
+    {
+        Options[Index] = new Option("Moyenne du prix d'une commande", ()=>ShowStatisticMeanAll());
+        Invoke();
+    }
+    
+    public void ShowStatisticMean()
+    {
+        string magasin = "\n"+string.Join(" euros \n", Database.Database.MeanPriceCommand())+" euros";
+        Options[Index] = new Option("La moyenne des ventes par magasin est :" + magasin , ()=>HideStatisticMean());
+        Invoke();
+    }
+    
+    public void HideStatisticMean()
+    {
+        Options[Index] = new Option("Moyenne des ventes par magasin", ()=>ShowStatisticMean());
         Invoke();
     }
 }
