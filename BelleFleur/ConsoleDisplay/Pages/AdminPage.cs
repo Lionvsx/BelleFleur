c9181827d8548f3843d96dfa4c56ea6e3a8bb92c
Leonard Roussard <lroussard@gmail.com>
2023-05-08 18:42:35 +0200

    feat: add admin page

---
+++ BelleFleur/ConsoleDisplay/Pages/AdminPage.cs
@@ -0,0 +1,44 @@
+namespace BelleFleur.ConsoleDisplay.Pages;
+
+public class AdminPage : Menu
+{
+    public AdminPage(string username)
+    {
+        var user = new Database.Structures.User(username);
+        Options = new List<Option>()
+        {
+            new("Vérifier les stocks", CheckStocks),
+            new("Toutes les commandes", CheckOrders),
+            new("Tous les utilisateurs", CheckUsers),
+            new("Tous les produits", CheckProducts),
+            new("Changer le mot de passe", ChangePassword),
+            new("Quitter", ExitMenu)
+        };
+        
+    }
+    
+    public void ChangePassword()
+    {
+        
+    }
+    
+    public void CheckStocks()
+    {
+        
+    }
+    
+    public void CheckOrders()
+    {
+        
+    }
+    
+    public void CheckUsers()
+    {
+        
+    }
+    
+    public void CheckProducts()
+    {
+        
+    }
+}
